b882daff9f3d7619c3d91717f105e76fcd77ace5
Ondiek Elijah Ochieng <oochieng@safaricom.co.ke>
2023-05-28 14:39:35 +0300

    chore: updated tests

--- users_frontend/tests/consumer.spec.js
+++ users_frontend/tests/consumer.spec.js
@@ -2,8 +2,8 @@ const path = require("path");
 const UserService = require("../client");
 const { PactV3, MatchersV3 } = require("@pact-foundation/pact");
 
-// Set up a server to run the consumer tests against
-const server = require("../../users_api/index");
+// // Set up a server to run the consumer tests against
+// const server = require("../../users_api/index");
 
 describe("Test", () => {
   // pact mock server url
@@ -18,69 +18,108 @@ describe("Test", () => {
     logLevel: "DEBUG",
   });
 
-  function createUser({
-    id,
-    name,
-    email,
-    age,
-    gender,
-    address,
-    phone,
-    company,
-  }) {
-    return {
-      id,
-      name,
-      email,
-      age,
-      gender,
-      address: {
-        street: address.street,
-        city: address.city,
-        state: address.state,
-        zip: address.zip,
-        country: address.country,
-      },
-      phone,
-      company,
-    };
-  }
-
-  const commonAddress = {
-    street: "123 Main St",
-    city: "New York",
-    state: "NY",
-    zip: "10001",
-    country: "USA",
-  };
-
-  const NEW_USER = createUser({
+  const NEW_USER = {
+    id: 4,
     name: "Mikey Mouse",
     email: "mikey@example.com",
     age: 22,
     gender: "Male",
-    address: commonAddress,
+    address: {
+      street: "123 Main St",
+      city: "New York",
+      state: "NY",
+      zip: "10001",
+      country: "USA",
+    },
     phone: "555-555-5555",
     company: "Meta Inc.",
-  });
+  };
 
-  const EXPECTED_BODY = createUser({
-    id: 1,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    age: 35,
-    gender: "Male",
-    address: commonAddress,
-    phone: "555-555-5555",
-    company: "Acme Inc.",
-  });
+  const EXPECTED_BODY_CREATE = {
+    message: "Entity with id 3 was successfully inserted",
+    status: "success",
+    user: {
+      address: {
+        city: "New York",
+        country: "USA",
+        state: "NY",
+        street: "123 Main St",
+        zip: "10001",
+      },
+      age: 22,
+      company: "Meta Inc.",
+      email: "mikey@example.com",
+      gender: "Male",
+      id: 3,
+      name: "Mikey Mouse",
+      phone: "555-555-5555",
+    },
+  };
 
-  const EXPECTED_BODY_CREATE = { ...NEW_USER, id: 3 };
+  const EXPECTED_BODY_GET = {
+    status: "success",
+    message: "Entity with id 2 successfully retrieved",
+    entity: {
+      id: 2,
+      name: "Jane Smith",
+      email: "janesmith@example.com",
+      age: 28,
+      gender: "Female",
+      address: {
+        street: "456 Oak St",
+        city: "Los Angeles",
+        state: "CA",
+        zip: "90001",
+        country: "USA",
+      },
+      phone: "555-555-1234",
+      company: "XYZ Corp.",
+    },
+  };
 
-  const EXPECTED_BODY_GET = createUser({
-    id: 2,
-    name: "Jane Smith",
-    email: "janesmith@example.com",
+  const EXPECTED_BODY_GET_ALL = {
+    status: "success",
+    message: "Entities successfully retrieved",
+    entities: [
+      {
+        id: 1,
+        name: "John Doe",
+        email: "johndoe@example.com",
+        age: 35,
+        gender: "Male",
+        address: {
+          street: "123 Main St",
+          city: "New York",
+          state: "NY",
+          zip: "10001",
+          country: "USA",
+        },
+        phone: "555-555-5555",
+        company: "Acme Inc.",
+      },
+      {
+        id: 2,
+        name: "Jane Smith",
+        email: "janesmith@example.com",
+        age: 28,
+        gender: "Female",
+        address: {
+          street: "456 Oak St",
+          city: "Los Angeles",
+          state: "CA",
+          zip: "90001",
+          country: "USA",
+        },
+        phone: "555-555-1234",
+        company: "XYZ Corp.",
+      },
+    ],
+  };
+
+  const BODY_UPDATE = {
+    id: 3,
+    name: "Mr Bob Johnson",
+    email: "bob@gmail.com",
     age: 28,
     gender: "Female",
     address: {
@@ -92,10 +131,27 @@ describe("Test", () => {
     },
     phone: "555-555-1234",
     company: "XYZ Corp.",
-  });
-
-  const BODY_UPDATE = { ...EXPECTED_BODY_CREATE, name: "The Mikey Mouse" };
-  const EXPECTED_BODY_UPDATE = BODY_UPDATE;
+  };
+  const EXPECTED_BODY_UPDATE = {
+    status: "success",
+    message: "Entity with id 3 was successfully updated",
+    entitiy: {
+      id: "3",
+      name: "Mr Bob Johnson",
+      email: "bob@gmail.com",
+      age: 28,
+      gender: "Female",
+      address: {
+        street: "456 Oak St",
+        city: "Los Angeles",
+        state: "CA",
+        zip: "90001",
+        country: "USA",
+      },
+      phone: "555-555-1234",
+      company: "XYZ Corp.",
+    },
+  };
 
   // Create a user
   it("test: create a user", () => {
@@ -116,7 +172,7 @@ describe("Test", () => {
     return provider.executeTest(() => {
       const users = new UserService(mock_server_url);
       return users.createUser(NEW_USER).then((response) => {
-        console.log("response", response);
+        console.log(response);
         expect(response).toEqual(EXPECTED_BODY_CREATE);
       });
     });
@@ -138,7 +194,7 @@ describe("Test", () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: MatchersV3.eachLike(EXPECTED_BODY),
+        body: EXPECTED_BODY_GET_ALL,
       });
     // Verify request
     return provider.executeTest(() => {
@@ -146,7 +202,7 @@ describe("Test", () => {
       const users = new UserService(mock_server_url);
       return users.getAllUsers().then((response) => {
         // Verify response
-        expect(response).toEqual([EXPECTED_BODY]);
+        expect(response).toEqual(EXPECTED_BODY_GET_ALL);
       });
     });
     // .finally(() => {
@@ -211,14 +267,13 @@ describe("Test", () => {
         path: "/users/3",
       })
       .willRespondWith({
-        status: 204,
-        headers: { "Content-Type": "application/json" },
+        status: 200,
+        headers: { "Content-Type": "text/html; charset=utf-8" },
       });
 
     return provider.executeTest(() => {
       const users = new UserService(mock_server_url);
       return users.deleteUser(3).then((response) => {
-        console.log("response", response);
         expect(response).toEqual("");
       });
     });
@@ -226,7 +281,7 @@ describe("Test", () => {
 });
 
 // Clean up after all tests
-afterAll(() => server.close());
+// afterAll(() => server.close());
 
 // test('adds 1 + 2 to equal 3', () => {
 //   expect(1 + 2).toBe(3);
