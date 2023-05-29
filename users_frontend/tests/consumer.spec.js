a52dbd7090bac0cf6c4977b3d5825bb2ccd7d0c0
Ondiek Elijah Ochieng <oochieng@safaricom.co.ke>
2023-05-29 05:39:42 +0300

    chore: add comments to demo consumer test

--- users_frontend/tests/consumer.spec.js
+++ users_frontend/tests/consumer.spec.js
@@ -1,15 +1,17 @@
+// To store the pact file in a different directory, we can use the dir option. 
+// The path module is used to resolve the path to the directory where the pact file will be stored. The dir option defaults to the current working directory.
 const path = require("path");
+// Import the client from where our app methods are defined. e.g. getAllUsers()
 const UserService = require("../client");
-const { PactV3, MatchersV3 } = require("@pact-foundation/pact");
+// Import Pact library
+const { PactV3 } = require("@pact-foundation/pact");
 
-// // Set up a server to run the consumer tests against
-// const server = require("../../users_api/index");
-
+// We use the describe function to group our tests together 
 describe("Test", () => {
-  // pact mock server url
+  // Pact mock server url
   const mock_port = 1234;
   const mock_server_url = "http://127.0.0.1:" + mock_port;
-  // pact instance
+  // Pact instance to create a 'pact' between the consumer and provider
   const provider = new PactV3({
     consumer: "users_frontend",
     provider: "users_api",
@@ -18,65 +20,7 @@ describe("Test", () => {
     logLevel: "DEBUG",
   });
 
-  const NEW_USER = {
-    id: 4,
-    name: "Mikey Mouse",
-    email: "mikey@example.com",
-    age: 22,
-    gender: "Male",
-    address: {
-      street: "123 Main St",
-      city: "New York",
-      state: "NY",
-      zip: "10001",
-      country: "USA",
-    },
-    phone: "555-555-5555",
-    company: "Meta Inc.",
-  };
-
-  const EXPECTED_BODY_CREATE = {
-    message: "Entity with id 3 was successfully inserted",
-    status: "success",
-    user: {
-      address: {
-        city: "New York",
-        country: "USA",
-        state: "NY",
-        street: "123 Main St",
-        zip: "10001",
-      },
-      age: 22,
-      company: "Meta Inc.",
-      email: "mikey@example.com",
-      gender: "Male",
-      id: 3,
-      name: "Mikey Mouse",
-      phone: "555-555-5555",
-    },
-  };
-
-  const EXPECTED_BODY_GET = {
-    status: "success",
-    message: "Entity with id 2 successfully retrieved",
-    entity: {
-      id: 2,
-      name: "Jane Smith",
-      email: "janesmith@example.com",
-      age: 28,
-      gender: "Female",
-      address: {
-        street: "456 Oak St",
-        city: "Los Angeles",
-        state: "CA",
-        zip: "90001",
-        country: "USA",
-      },
-      phone: "555-555-1234",
-      company: "XYZ Corp.",
-    },
-  };
-
+  // Define the expected response body
   const EXPECTED_BODY_GET_ALL = {
     status: "success",
     message: "Entities successfully retrieved",
@@ -116,71 +60,9 @@ describe("Test", () => {
     ],
   };
 
-  const BODY_UPDATE = {
-    id: 3,
-    name: "Mr Bob Johnson",
-    email: "bob@gmail.com",
-    age: 28,
-    gender: "Female",
-    address: {
-      street: "456 Oak St",
-      city: "Los Angeles",
-      state: "CA",
-      zip: "90001",
-      country: "USA",
-    },
-    phone: "555-555-1234",
-    company: "XYZ Corp.",
-  };
-  const EXPECTED_BODY_UPDATE = {
-    status: "success",
-    message: "Entity with id 3 was successfully updated",
-    entitiy: {
-      id: "3",
-      name: "Mr Bob Johnson",
-      email: "bob@gmail.com",
-      age: 28,
-      gender: "Female",
-      address: {
-        street: "456 Oak St",
-        city: "Los Angeles",
-        state: "CA",
-        zip: "90001",
-        country: "USA",
-      },
-      phone: "555-555-1234",
-      company: "XYZ Corp.",
-    },
-  };
-
-  // Create a user
-  it("test: create a user", () => {
-    // interaction
-    provider
-      .uponReceiving("a POST request to create a user")
-      .withRequest({
-        method: "POST",
-        path: "/users",
-        body: NEW_USER,
-      })
-      .willRespondWith({
-        status: 201,
-        headers: { "Content-Type": "application/json" },
-        body: EXPECTED_BODY_CREATE,
-      });
-
-    return provider.executeTest(() => {
-      const users = new UserService(mock_server_url);
-      return users.createUser(NEW_USER).then((response) => {
-        console.log(response);
-        expect(response).toEqual(EXPECTED_BODY_CREATE);
-      });
-    });
-  });
-
   // Fetch all users
   it("test: fetch all users", () => {
-    // interaction
+    // Arrange: Setup our expected interactions between the consumer and provider
     provider
       // Set up expected request
       .uponReceiving("a GET request to get all users")
@@ -196,93 +78,15 @@ describe("Test", () => {
         },
         body: EXPECTED_BODY_GET_ALL,
       });
+
     // Verify request
     return provider.executeTest(() => {
       // Make request to the mock server
       const users = new UserService(mock_server_url);
       return users.getAllUsers().then((response) => {
-        // Verify response
+        // Assert: Verify response
         expect(response).toEqual(EXPECTED_BODY_GET_ALL);
-      });
-    });
-    // .finally(() => {
-    //   server.close();
-    // });
-  });
-
-  // Fetch single user
-  it("test: fetch a single user", () => {
-    // interaction
-    provider
-      .uponReceiving("a GET request to get a single user")
-      .withRequest({
-        method: "GET",
-        path: "/users/2",
-      })
-      .willRespondWith({
-        status: 200,
-        headers: { "Content-Type": "application/json" },
-        body: EXPECTED_BODY_GET,
-      });
-
-    return provider.executeTest(() => {
-      const users = new UserService(mock_server_url);
-      return users.getUser(2).then((response) => {
-        expect(response).toEqual(EXPECTED_BODY_GET);
-      });
-    });
-  });
-
-  // Update a user
-  it("test: update a user", () => {
-    // interaction
-    provider
-      .uponReceiving("a PUT request to update a user")
-      .withRequest({
-        method: "PUT",
-        path: "/users/3",
-        body: BODY_UPDATE,
-      })
-      .willRespondWith({
-        status: 200,
-        headers: { "Content-Type": "application/json" },
-        body: EXPECTED_BODY_UPDATE,
-      });
-
-    return provider.executeTest(() => {
-      const users = new UserService(mock_server_url);
-      return users.updateUser(BODY_UPDATE).then((response) => {
-        expect(response).toEqual(EXPECTED_BODY_UPDATE);
-      });
-    });
-  });
-
-  // Delete a user
-  it("test: delete a user", () => {
-    // interaction
-    provider
-      .uponReceiving("a DELETE request to delete a user")
-      .withRequest({
-        method: "DELETE",
-        path: "/users/3",
-      })
-      .willRespondWith({
-        status: 200,
-        headers: { "Content-Type": "text/html; charset=utf-8" },
-      });
-
-    return provider.executeTest(() => {
-      const users = new UserService(mock_server_url);
-      return users.deleteUser(3).then((response) => {
-        expect(response).toEqual("");
       });
     });
   });
 });
-
-// Clean up after all tests
-// afterAll(() => server.close());
-
-// test('adds 1 + 2 to equal 3', () => {
-//   expect(1 + 2).toBe(3);
-// });
